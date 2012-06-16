b5594b930f8a7e8d30be4cd8266f607451ed6709
suisho <suisho000000@gmail.com>
2012-06-17 03:52:57 +0900

    user_idが0で名無しさんになるように変更

--- app/models/copipe.rb
+++ app/models/copipe.rb
@@ -1,7 +1,11 @@
+
 class Copipe < ActiveRecord::Base
   attr_accessible :body, :title, :user_id
 
-  def get_body_html()
-
+  def anonymous?
+    if self.user_id == 0
+      return true
+    end
+    return false
   end
 end
