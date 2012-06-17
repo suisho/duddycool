52ec1675cd7fce811f7ca1a750470070301fafc1
suisho <suisho000000@gmail.com>
2012-06-18 00:44:39 +0900

    ヘルパーに2ch名部分を加工するものを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,3 @@
 module ApplicationHelper
-  def format_article(text)
-    text = text.dup
-    text = html_escape(text)
-    text = text.gsub(/\r\n|\r|\n/, "<br>").html_safe
-    content_tag(:div, text, :class => "article_body")
-  end
+  
 end
