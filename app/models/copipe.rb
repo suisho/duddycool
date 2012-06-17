4592819fe78f65bf188fc566f172031f62a59bb8
suisho <suisho000000@gmail.com>
2012-06-18 02:25:31 +0900

    タグ対応

--- app/models/copipe.rb
+++ app/models/copipe.rb
@@ -1,6 +1,8 @@
 
 class Copipe < ActiveRecord::Base
   attr_accessible :body, :title, :user_id
+  has_many :copipe_tags
+  has_many :tags, :through => :copipe_tags
 
   def anonymous?
     if self.user_id == 0
