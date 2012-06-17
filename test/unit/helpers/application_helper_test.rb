7c6b3fc90bdd434cc35d5c33ad81d32df72dc047
suisho <suisho000000@gmail.com>
2012-06-18 02:21:36 +0900

    メソッド移動

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -1,23 +1,5 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  include ERB::Util
-  test 'format_article' do
-    article = "aaaa
-bbbb"
-    assert_equal '<div class="article_body">aaaa<br>bbbb</div>', format_article(article)
-  end
 
-  test 'format_article_sanitize' do
-    warn_article = "aa<br>bb"
-    assert_equal '<div class="article_body">aa&lt;br&gt;bb</div>', format_article(warn_article)
-  end
-
-  test 'format_article_multi_br' do
-    multi_br =
-"1
-
-2"
-    assert_equal '<div class="article_body">1<br><br>2</div>', format_article(multi_br)
-  end
 end
