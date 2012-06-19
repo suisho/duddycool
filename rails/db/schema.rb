930eb50f3562cbc453db0180241d07590572b56a
suisho <suisho000000@gmail.com>
2012-06-20 02:34:09 +0900

    Remove old user table

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120617164220) do
+ActiveRecord::Schema.define(:version => 20120619172452) do
 
   create_table "copipe_tags", :force => true do |t|
     t.integer  "copipe_id"
@@ -35,12 +35,5 @@ ActiveRecord::Schema.define(:version => 20120617164220) do
   end
 
   add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
-
-  create_table "users", :force => true do |t|
-    t.string   "twitter_id"
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
 end
